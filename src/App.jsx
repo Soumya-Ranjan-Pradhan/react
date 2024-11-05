f954f4f180d14eb83d7d6ae56eefc1b105d1f74b
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2024-11-05 18:43:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const App = () => {
+  // const myName = "hello";
+  // let a = 10;
+  // let b = 20;
+  // function hello(a, b) {
+  //   return a + b;
+  // }
+
+  // const userOBJ = {
+  //   myName: "Hello",
+  //   age: 23,
+  //   email: "soumyapradhan63711@gmail.com",
+  // };
+
+  const names = "MyName"
+
+  // const names = ["Sum","Peter","Sum1"]
+  return (
+    <>
+      {/* <h1>Hello World</h1>
+      <h1>{myName}</h1>
+      <h2>{a + b}</h2>
+      <h3>{hello(12,13)}</h3> */}
+      {/* <h1>{userOBJ.email}</h1> */}
+      <h2>{names[0]}</h2>
+      <input type="text" value={names} />
+    </>
+  );
+};
+
+export default App;
